5bd7bc895cbd7fb0a3bf887d31ef5b81b1d53b57
Chris Brown <chris@ourbigadventure.com>
2019-12-10 10:46:17 -0700

    Initial commit

---
+++ MovieDatabase/MovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Chris Brown on 12/10/19.
+//  Copyright © 2019 Chris Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
